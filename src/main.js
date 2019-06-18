f1acc7585ca6a6e9c279ef4da262597a5a4d4e10
gongph <gongph@foxmail.com>
2019-06-18 19:28:29 +0800

    fix: 修复键盘样式问题

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import 'normalize.css'
+import '@/style/index.scss'
 
 Vue.config.productionTip = false
 
