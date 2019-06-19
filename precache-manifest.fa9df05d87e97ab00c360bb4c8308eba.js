43505b25f6cafc803dc6458709f504d13fd6d4dc
gongph <gongph@foxmail.com>
2019-06-19 14:05:45 +0800

    deploy

---
+++ precache-manifest.fa9df05d87e97ab00c360bb4c8308eba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-mand-mobile-app/robots.txt"
+  },
+  {
+    "revision": "81bb1388b386daf0dea5",
+    "url": "/vue-mand-mobile-app/js/chunk-vendors.de3fb458.js"
+  },
+  {
+    "revision": "0530c250e73af905a055",
+    "url": "/vue-mand-mobile-app/js/app.c80648c7.js"
+  },
+  {
+    "revision": "d96168cedc847fdfc4ff944470f02d83",
+    "url": "/vue-mand-mobile-app/index.html"
+  },
+  {
+    "revision": "cd4ef53b118370ae4e579ddbe93c6b85",
+    "url": "/vue-mand-mobile-app/img/preview.jpg"
+  },
+  {
+    "revision": "81bb1388b386daf0dea5",
+    "url": "/vue-mand-mobile-app/css/chunk-vendors.8c800bd5.css"
+  },
+  {
+    "revision": "0530c250e73af905a055",
+    "url": "/vue-mand-mobile-app/css/app.4bc2066b.css"
+  }
+];
